616842e086eda7d4c039ae2113f2704cca3cb620
KM1L0-717 <capm717@gmail.com>
2025-06-12 19:17:33 -0500

    hu4

--- Automatizacion/Historiasdeusuarios/src/main/java/co/com/AutomatizacionDancePage/tasks/Hu4Tasks.java
+++ Automatizacion/Historiasdeusuarios/src/main/java/co/com/AutomatizacionDancePage/tasks/Hu4Tasks.java
@@ -12,6 +12,7 @@ import net.serenitybdd.screenplay.waits.WaitUntil;
 import java.util.List;
 
 import static co.com.AutomatizacionDancePage.userinterface.ClassesUserInterface.*;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class Hu4Tasks implements Task {
@@ -24,6 +25,8 @@ public class Hu4Tasks implements Task {
         actor.attemptsTo(
                 Click.on(BOTON_FECHA2),
                 Click.on(BOTON_INFORMACION),
+                WaitUntil.the(MODAL_CERRAR, isVisible()).forNoMoreThan(10).seconds(),
+                WaitUntil.the(MODAL_CERRAR, isEnabled()).forNoMoreThan(10).seconds(),
                 Click.on(MODAL_CERRAR),
                 Click.on(BOTON_FECHA1),
                 Click.on(BOTON_INFORMACION)
