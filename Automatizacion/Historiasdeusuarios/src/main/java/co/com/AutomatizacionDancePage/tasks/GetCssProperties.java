53cbbc7b13067866ff91903b74951d57ad8112d1
miguelcoronado003 <montanomiguel2013@gmail.com>
2025-06-25 21:07:55 -0500

    arreglo de errores

--- Automatizacion/Historiasdeusuarios/src/main/java/co/com/AutomatizacionDancePage/tasks/GetCssProperties.java
+++
@@ -1,9 +0,0 @@
-package co.com.AutomatizacionDancePage.tasks;
-
-import org.openqa.selenium.WebElement;
-
-public class GetCssProperties {
-    public static String getProperty(WebElement element, String property){
-        return element.getCssValue(property);
-    }
-}
--- Automatizacion/Historiasdeusuarios/src/main/java/co/com/AutomatizacionDancePage/userinterface/VistaClases.java
+++
@@ -1,8 +0,0 @@
-package co.com.AutomatizacionDancePage.userinterface;
-
-import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.pages.PageObject;
-
-@DefaultUrl(value = "http://localhost:5173/Admin/programacion_clases")
-public class VistaClases extends PageObject {
-}
